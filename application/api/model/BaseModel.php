4f24da33fa4e9a8e4eb43de3f72c2ef0e0ef9abc
laowangplus <asd415813765@gmail.com>
2019-11-05 12:37:57 +0800

    first commit

---
+++ application/api/model/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 老王专用
+ * Date: 2019/5/31
+ * Time: 22:07
+ */
+
+namespace app\api\model;
+
+
+use think\Model;
+
+class BaseModel extends Model
+{
+    protected $user_id = 1;
+}
